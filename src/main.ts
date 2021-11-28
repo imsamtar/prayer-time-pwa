48c55db746b2039114b853cf6e723f82f1b51e57
Sameer Tariq <imsamtar@gmail.com>
2021-11-29 01:39:13 +0500

    update

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,25 @@
 import App from './App.svelte'
 
 const app = new App({
-  target: document.getElementById('app')
+  target: document.getElementById('app'),
 })
 
 export default app
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function () {
+    navigator.serviceWorker.register('/service-worker.js').then(
+      function (registration) {
+        // Registration was successful
+        console.log(
+          'ServiceWorker registration successful with scope: ',
+          registration.scope,
+        )
+      },
+      function (err) {
+        // registration failed :(
+        console.log('ServiceWorker registration failed: ', err)
+      },
+    )
+  })
+}
